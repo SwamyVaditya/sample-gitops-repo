7995a367258afdd1df47c47f9b92c07ada217a4b
Swamy Vaditya <swamyvaditya@gmail.com>
2025-08-02 02:25:33 +0530

    new feature

--- sample-app/server.js
+++ sample-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Congratulations! CI workflow is running fine!!!");
+  res.send("Testing CI workflow!!!");
 });
 
 const port = process.env.PORT || 3000;
