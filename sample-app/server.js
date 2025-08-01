8e3f27bb3b8c6b00d34767325dd23348ec929d88
Swamy Vaditya <swamyvaditya@gmail.com>
2025-08-02 03:19:14 +0530

    Updated server.js to test CI/CD workflow

--- sample-app/server.js
+++ sample-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Testing CI workflow!!!");
+  res.send("Testing CI/CD workflow!!!");
 });
 
 const port = process.env.PORT || 3000;
