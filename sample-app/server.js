a3fc579d813cf3c6127b2a62bd0a60ecb4b4ebed
Swamy Vaditya <swamyvaditya@gmail.com>
2025-08-19 00:14:16 +0530

    Testing bump:minor

--- sample-app/server.js
+++ sample-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Testing workflows - GitHub PR Labeling and ArgoCD");
+  res.send("Testing workflows - PR Labeling bump:minor");
 });
 
 const port = process.env.PORT || 3000;
