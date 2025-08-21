bd2dee6148b0698bb16bce52b3b19586c573f5aa
Swamy Vaditya <swamyvaditya@gmail.com>
2025-08-22 04:36:54 +0530

    Update server.js

--- sample-app/server.js
+++ sample-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Testing workflows files !!!");
+  res.send("Testing workflows files - 1");
 });
 
 const port = process.env.PORT || 3000;
